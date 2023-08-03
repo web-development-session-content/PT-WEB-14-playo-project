29edf6e912867e52f7274e94186cf6a4ae8938ef
Nishal Barman <nishalbarman@gmail.com>
2023-08-03 15:47:07 +0530

    Few pages working

--- components/navbar.js
+++ components/navbar.js
@@ -1,14 +1,14 @@
-function navbar(){
-    return `<div id="navbar">
+function navbar() {
+  return `<div id="navbar">
                 <a id="active" href="index.html">
-                    <img id="logo" src="https://playo-website.gumlet.net/logo/playo-logo-header-website.png?auto=compress,format&amp;q=90" alt="">
+                    <img id="logo" src="https://playo-website.gumlet.io/playo-website-v2/Logo+with+Trademark_Filled.png" alt="">
                 </a>
 
-                <ul id="nav_options">
-                    <li><a href="venues.html"><span class="link-btn">Book Venues</span></a></li>
-                    <li><a href="activities.html"><span class="link-btn">Activites</span></a></li>
-                    <li><a href="blog.html"><span class="link-btn">Blog</span></a></li>
-                </ul>
+                <div id="nav_options">
+                    <a href="venues.html"><span class="link-btn">Book Venues</span></a>
+                    <a href="activities.html"><span class="link-btn">Activites</span></a>
+                    <a href="blog.html"><span class="link-btn">Blog</span></a>
+                </div>
             </div>`;
 }
 
